c4bd392da9b77af8e9c42e54e523896775435d40
komkpl <komhas@icloud.com>
2025-04-16 13:26:09 +0900

    add: heicからjpgに変換するコードを書き、動作確認をした

--- heic_to_others.py
+++ heic_to_others.py
@@ -4,10 +4,10 @@ import os
 from pathlib import Path
 
 def make_output_path(input_path: Path, output_extention: str) -> Path:
-    return os.path.splitext(input_path)[0] + "." + output_extention
+    return Path(os.path.splitext(input_path)[0] + "." + output_extention)
 
-def convert_heic_to_others(input_path: Path, output_extention: str) -> Image:
-    heic_file = pillow_heif.read_heif(input_path)
+def convert_heic_to_others(input_path: Path, output_extention: str) -> None:
+    heif_file = pillow_heif.read_heif(input_path)
     image = Image.frombytes(
         heif_file.mode,
         heif_file.size,
@@ -16,5 +16,10 @@ def convert_heic_to_others(input_path: Path, output_extention: str) -> Image:
     )
 
     output_path = make_output_path(input_path, output_extention)
-    image.save(output_path, output_extention.upper())
+    image.save(output_path)
     print(f"変換完了: {output_path}")
+
+if __name__ == "__main__":
+    input_path = Path("example.HEIC")
+    output_extention = "jpg"  # 変換先の拡張子を指定
+    convert_heic_to_others(input_path, output_extention)
